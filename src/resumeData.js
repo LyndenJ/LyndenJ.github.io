d502fc7ac236ad01693cdf425dace20752b77a7d
Lynden Jones <lynden@Lyndens-MacBook-Pro.local>
2022-09-11 22:31:42 -0600

    Reposity initialized

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Lynden Jones",
+    "role": "third year student at the University of Alberta persuing a co-op bachelors degree in software engineering",
+    "linkedinId":" Lynden Jones",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I am an aspiring fullstack web devoloper who thrives in an environment where I can learn and absorb lots of information to help refine my craft",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/lynden-jones-39022a226/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/LyndenJ",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I'm from Calgary, Alberta, Canada and have been teaching myself software development ever since I owned my first computer. Fullstack web development is my bread and butter and I have aspirations to move into Machine Learning and Artificial Intelligence. I am currently on a study term at Ualberta looking to put my skillset towards an internship beginning in January 2023",
+    "address":"+1 (587) 577-8000",
+    "website":"lyndenjones0@gmail.com",
+    "education":[
+      {
+        "UniversityName":"University of Alberta",
+        "specialization":"B.Sc Computer Engineering Software Option Co-op program",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2025",
+        "Achievements":"Awarded a Bruce A. Johnston Scholarship for Engineering and Leadership",
+      },
+      // {
+      //   "UniversityName":"Some University",
+      //   "specialization":"Some specialization",
+      //   "MonthOfPassing":"Jan",
+      //   "YearOfPassing":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "work":[
+      {
+        "CompanyName":"Neo Financial",
+        "specialization":"Software Test Engineer",
+        "MonthOfLeaving":"August",
+        "YearOfLeaving":"2022",
+        "Achievements":"During my time with the growth team, we increased savings application conversion by ~18% as well as improved mortgage lead acquisition rates by ~150x"
+      },
+      {
+        "CompanyName":"Westech Healthcare ltd.",
+        "specialization":"Database Engineer",
+        "MonthOfLeaving":"August",
+        "YearOfLeaving":"2021",
+        "Achievements":"Added data input validation facing the sales representatives which improved data quality and helped facilitate large amounts of data to be entered by sales representatives"
+      }
+    ],
+    "skillsDescription":"Some of my notable skills include:",
+    "skills":[
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Typescript"
+      },
+      {
+        "skillname":"java"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Navigation System",
+        "description":"Client-Server demo",
+        "imgurl":"images/portfolio/navigationSystem.jpeg"
+      },
+      {
+        "name":"Naive Bayes Classifier",
+        "description":"Weather Classification",
+        "imgurl":"images/portfolio/classifier.jpeg"
+      },
+      {
+        "name":"Compression Algorithm",
+        "description":"Huffman Encoding",  
+        "imgurl":"images/portfolio/huffmanEncoding.webp"
+      },
+      {
+        "name":"This Personal Website!",
+        "description":"",
+        "imgurl":"images/portfolio/youAreHere.jpeg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Those who can imagine anything can create the impossible",
+        "name":"Alan Turing"
+      },
+      {
+        "description":"Computer science is no more about computers than astronomy is about telescopes",
+        "name":"E.W. Dijkstra"
+      }
+    ]
+  }
+  
+  export default resumeData
